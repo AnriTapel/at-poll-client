6f86872558296f8bc075278f961e7c708ffdf7c3
AnriTapel <anri.snep@gmail.com>
2024-06-14 14:13:40 +0300

    initial setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
+import {provideHttpClient, withFetch} from "@angular/common/http";
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+
+const socketConfig: SocketIoConfig = {
+  options: {}, url: "http://localhost:3000/"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(SocketIoModule.forRoot(socketConfig))
+  ]
 };
